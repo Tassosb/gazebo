e230de4c031b5a894b022347d0e28c59acd2aa3e
Tassosb <tassosbareiss@gmail.com>
2017-03-21 22:56:07 -0400

    Remove unnecessary require statements

--- demo/boot.rb
+++ demo/boot.rb
@@ -2,9 +2,6 @@ require_relative '../lib/gazebo'
 
 Gazebo.root = File.expand_path(File.dirname(__FILE__))
 
-require_relative 'app/models/cat'
-require_relative 'app/models/human'
-require_relative 'app/controllers/cats_controller'
 require_relative 'config/routes'
 
 Gazebo.boot
