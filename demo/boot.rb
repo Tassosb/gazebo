f15812f26646c1972a5eba8effa6e7b2bb345964
Tassosb <tassosbareiss@gmail.com>
2017-03-25 20:28:01 -0400

    Automatically fetch user drawn routes

--- demo/boot.rb
+++ demo/boot.rb
@@ -2,6 +2,4 @@ require_relative '../lib/gazebo'
 
 Gazebo.root = File.expand_path(File.dirname(__FILE__))
 
-require_relative 'config/routes'
-
 Gazebo.boot
--- lib/gazebo.rb
+++ lib/gazebo.rb
@@ -14,6 +14,8 @@ module Gazebo
   def self.boot
     seed! if ARGV[0] == 'seed'
 
+    fetch_routes!
+
     app = Proc.new do |env|
       req = Rack::Request.new(env)
       res = Rack::Response.new
@@ -37,9 +39,17 @@ module Gazebo
     const_set("ROOT", root)
   end
 
+  def self.fetch_routes!
+    file = File.join(ROOT, "config/routes.rb")
+
+    File.open(file) do |f|
+      self.class_eval(f.read)
+    end
+  end
+
   def self.seed!
     file = File.join(ROOT, "db/seeds.rb")
-    
+
     File.open(file) do |f|
       self.class_eval(f.read)
     end
