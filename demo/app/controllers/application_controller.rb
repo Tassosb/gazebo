5144ae7ef813e66094630e2699a0a780110aaa75
Tassos Bareiss <tassosbareiss@gmail.com>
2017-03-27 18:09:24 -0400

    Remove demo

--- demo/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionCondor::Base
-
-  def current_user
-    @current_user ||= User.find_by(
-      session_token: session[:session_token]
-    )
-  end
-
-end
--- demo/boot.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/gazebo'
-
-Gazebo.root = File.expand_path(File.dirname(__FILE__))
-
-Gazebo.boot
